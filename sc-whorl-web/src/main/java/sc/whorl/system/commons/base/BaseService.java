c196cc99e4ea2ac0c81ea57da82faf189d9b9c9b
qisensen <qisensen@encdata.cn>
2019-10-31 18:09:19 +0800

    更新

--- sc-whorl-web/src/main/java/sc/whorl/system/commons/base/BaseService.java
+++ sc-whorl-web/src/main/java/sc/whorl/system/commons/base/BaseService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 import sc.whorl.system.commons.MyMapper;
-import sc.whorl.web.vo.system.UpMenuRequest;
 
 
 public abstract class BaseService<M extends MyMapper<T>, T> {
@@ -16,82 +15,82 @@ public abstract class BaseService<M extends MyMapper<T>, T> {
     }
 
 
-    public T selectOne(T entity) {
+    protected T selectOne(T entity) {
         return mapper.selectOne(entity);
     }
 
 
-    public T selectByPrimaryKey(Object id) {
+    protected T selectByPrimaryKey(Object id) {
         return mapper.selectByPrimaryKey(id);
     }
 
 
-    public List<T> selectList(T entity) {
+    protected List<T> selectList(T entity) {
         return mapper.select(entity);
     }
 
 
-    public List<T> selectListAll() {
+    protected List<T> selectListAll() {
         return mapper.selectAll();
     }
 
 
-    public Long selectCount(T entity) {
+    protected Long selectCount(T entity) {
         return new Long(mapper.selectCount(entity));
     }
 
 
-    public void insert(T entity) {
+    protected void insert(T entity) {
         mapper.insert(entity);
     }
 
 
-    public void insertSelective(T entity) {
+    protected void insertSelective(T entity) {
         mapper.insertSelective(entity);
     }
 
 
-    public void delete(T entity) {
+    protected void delete(T entity) {
         mapper.delete(entity);
     }
 
 
-    public void deleteByPrimaryKey(Object id) {
+    protected void deleteByPrimaryKey(Object id) {
         mapper.deleteByPrimaryKey(id);
     }
 
 
-    public void updateByPrimaryKey(T entity) {
+    protected void updateByPrimaryKey(T entity) {
         mapper.updateByPrimaryKey(entity);
     }
 
 
-    public void updateByPrimaryKeySelective(T entity) {
+    protected void updateByPrimaryKeySelective(T entity) {
         mapper.updateByPrimaryKeySelective(entity);
 
     }
 
-    public List<T> selectByExample(Object example) {
+    protected List<T> selectByExample(Object example) {
         return mapper.selectByExample(example);
     }
 
-    public int selectCountByExample(Object example) {
+    protected int selectCountByExample(Object example) {
         return mapper.selectCountByExample(example);
     }
 
-    public int updateByExampleSelective(T entity, Object example) {
+    protected int updateByExampleSelective(T entity, Object example) {
         return mapper.updateByExampleSelective(entity, example);
     }
 
-    public boolean existsWithPrimaryKey(Object primaryKey) {
+    protected boolean existsWithPrimaryKey(Object primaryKey) {
         return mapper.existsWithPrimaryKey(primaryKey);
     }
 
-    public T selectOneByExample(Object example) {
+    protected T selectOneByExample(Object example) {
         return mapper.selectOneByExample(example);
     }
 
-    public int insertList(List<T> recordList) {
+    protected int insertList(List<T> recordList) {
         return mapper.insertList(recordList);
     }
 
